8837403764e06edcc2eccabb896ea8c176e029e9
CGJI58 <leemsk22@naver.com>
2022-02-08 18:15:15 +0900

    Update 1.4
    header 인터페이스 변경함.
    todolist 배경 어둡게 변경함.

--- js/clock.js
+++ js/clock.js
@@ -1,13 +1,26 @@
+const today = document.querySelector("#date");
 const clock = document.querySelector("#clock");
 
+function getToday() {
+  const dateInfo = new Date();
+  const year = String(dateInfo.getFullYear()).padStart(4, "0");
+  const month = String(dateInfo.getMonth() + 1).padStart(2, "0");
+  const date = String(dateInfo.getDate()).padStart(2, "0");
+  today.innerText = `${year}-${month}-${date}`;
+  console.log(today);
+}
+
 function getClock() {
-  const date = new Date();
-  const hours = String(date.getHours()).padStart(2, "0");
-  const minutes = String(date.getMinutes()).padStart(2, "0");
-  const seconds = String(date.getSeconds()).padStart(2, "0");
+  const time = new Date();
+  const hours = String(time.getHours()).padStart(2, "0");
+  const minutes = String(time.getMinutes()).padStart(2, "0");
+  const seconds = String(time.getSeconds()).padStart(2, "0");
 
   clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
+
+getToday();
+setInterval(getToday, 1000);
 
 getClock();
 setInterval(getClock, 1000);
