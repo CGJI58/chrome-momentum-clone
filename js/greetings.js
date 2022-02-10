5c5ffd791488cef15fb849d669f6e7a71e09ddf1
CGJI58 <leemsk22@naver.com>
2022-02-11 00:02:16 +0900

    Update 1.6
    greetings js파일에서 배경이미지 관련 기능, 로그인폼 관련 기능, 인사문구 보여주는 기능으로 분류하여 오브젝트로 구분하였음.

--- js/greetings.js
+++ js/greetings.js
@@ -1,38 +1,60 @@
+const bgImage = document.querySelector("#background-image");
 const loginForm = document.querySelector(".login-form");
 const loginInput = document.querySelector(".login-form input");
 const greeting = document.querySelector("#greeting");
-const bgImage = document.querySelector("#background-image");
-
-const HIDDEN_CLASSNAME = "hidden";
-const APPEAR_CLASSNAME = "appear";
+const savedUsername = localStorage.getItem("USERNAME_KEY");
 const IMGBEFORELOGIN_CLASSNAME = "img-before-login";
 const IMGAFTERLOGIN_CLASSNAME = "img-after-login";
+const HIDDEN_CLASSNAME = "hidden";
+const APPEAR_CLASSNAME = "appear";
 const USERNAME_KEY = "username";
 
-const savedUsername = localStorage.getItem("USERNAME_KEY");
+const bgImageManager = {
+  logIn: function logInImage(arg) {
+    arg.classList.add(IMGAFTERLOGIN_CLASSNAME);
+    arg.classList.remove(IMGBEFORELOGIN_CLASSNAME);
+  },
+  logOut: function logOutImage(arg) {
+    arg.classList.add(IMGBEFORELOGIN_CLASSNAME);
+    arg.classList.remove(IMGAFTERLOGIN_CLASSNAME);
+  },
+};
+
+const greetingManager = {
+  welcomeTextOn: function welcome(arg) {
+    greeting.innerText = `Hello ${arg}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+  },
+};
+
+const logInfoManager = {
+  formOn: function formOn(arg) {
+    arg.classList.add(APPEAR_CLASSNAME);
+    arg.classList.remove(HIDDEN_CLASSNAME);
+    arg.addEventListener("submit", onLoginSubmit);
+  },
+  formOff: function formOff(arg) {
+    arg.classList.add(HIDDEN_CLASSNAME);
+    arg.classList.remove(APPEAR_CLASSNAME);
+  },
+};
 
 if (savedUsername === null) {
-  loginForm.classList.remove(HIDDEN_CLASSNAME);
-  loginForm.classList.add(APPEAR_CLASSNAME);
-  loginForm.addEventListener("submit", onLoginSubmit);
+  logInfoManager.formOn(loginForm);
 } else {
-  bgImage.classList.remove(IMGBEFORELOGIN_CLASSNAME);
-  bgImage.classList.add(IMGAFTERLOGIN_CLASSNAME);
+  bgImageManager.logIn(bgImage);
   paintGreetings(savedUsername);
 }
 
 function onLoginSubmit(arg) {
   arg.preventDefault();
-  loginForm.classList.add(HIDDEN_CLASSNAME);
-  loginForm.classList.remove(APPEAR_CLASSNAME);
-  bgImage.classList.add(IMGAFTERLOGIN_CLASSNAME);
-  bgImage.classList.remove(IMGBEFORELOGIN_CLASSNAME);
+  bgImageManager.logIn(bgImage);
+  logInfoManager.formOff(loginForm);
   const typedUsername = loginInput.value;
   localStorage.setItem("USERNAME_KEY", typedUsername);
   paintGreetings(typedUsername);
 }
 
 function paintGreetings(username) {
-  greeting.innerText = `Hello ${username}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+  greetingManager.welcomeTextOn(username);
 }
