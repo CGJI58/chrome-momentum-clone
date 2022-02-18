6b47221247847c8b4108ae829fba17a93b7a7f67
CGJI58 <leemsk22@naver.com>
2022-02-18 16:47:58 +0900

    Update 2.0
    todo list의 check-button이 localStorage에 반영되도록 수정함.

--- js/todo.js
+++ js/todo.js
@@ -30,17 +30,22 @@ function handleToDoSubmit(arg) {
 function paintToDo(newToDo) {
   const li = document.createElement("li");
   li.id = newToDo.id;
-  const delButton = document.createElement("button");
-  delButton.innerText = "X";
+
   const checkButton = document.createElement("button");
   checkButton.innerText = "V";
+  li.appendChild(checkButton);
+  checkButton.addEventListener("click", checkToDo);
+
+  const delButton = document.createElement("button");
+  delButton.innerText = "X";
+  li.appendChild(delButton);
+  delButton.addEventListener("click", deleteToDo);
+
   const span = document.createElement("span");
   span.innerText = newToDo.text;
-  delButton.addEventListener("click", deleteToDo);
-  checkButton.addEventListener("click", checkToDo);
-  li.appendChild(delButton);
-  li.appendChild(checkButton);
+  span.classList = newToDo.class;
   li.appendChild(span);
+
   toDoList.appendChild(li);
 }
 
@@ -52,13 +57,26 @@ function deleteToDo(arg) {
   const li = arg.target.parentNode;
   toDos = toDos.filter((item) => item.id !== parseInt(li.id));
   saveToDos();
-
   li.remove();
 }
 
 function checkToDo(arg) {
-  const todo = arg.target.parentNode.querySelector("span");
-  todo.classList.toggle("checked-todo");
+  const checkedTodo = arg.target.parentNode;
+  const checkedTodoSpan = checkedTodo.querySelector("span");
+  checkedTodoSpan.classList.toggle("checked-todo");
+
+  toDos.filter(function addCheck(item) {
+    if (item.id == checkedTodo.id) {
+      if (item.class == undefined) item.class = "checked-todo";
+      else if (item.class == "checked-todo") item.class = undefined;
+      else
+        console.log(
+          "todo list의 span에 checked-todo 외의 class를 추가하면 정상적으로 작동 안합니다."
+        );
+    }
+    return true;
+  });
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
