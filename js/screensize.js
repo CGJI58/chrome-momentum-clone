25f0eed50a39e1b00c57da7de1ef6deb6be78e08
CGJI58 <leemsk22@naver.com>
2022-02-20 17:37:35 +0900

    Update 2.52
    2.52버전이 핸드폰에서 키보드 나타난 상태에서 화면을 스크롤할 수 있는지 확인해볼 것.

--- js/screensize.js
+++ js/screensize.js
@@ -1,11 +1,4 @@
-console.log(window.outerWidth, window.outerHeight);
-console.log(window.innerWidth, window.innerHeight);
-
 const bodySize = document.querySelector("body");
-// console.log(document);
 
-bodySize.style.width = `${window.innerWidth}px`;
+// bodySize.style.width = `${window.innerWidth}px`;
 bodySize.style.height = `${window.innerHeight}px`;
-
-// bodySize.onresize = "parent.resizeTo(500,400)";
-// bodySize.onload = "parent.resizeTo(500,400)";
